00c3e3a8bd283c81387de7fd19f9ad237e309793
RuBaPr <118204785+RuBaPr@users.noreply.github.com>
2025-02-13 10:47:17 +0100

    Plantilla de proyecto
    En este commit subo la plantilla general del proyecto

---
+++ src/main/java/es/urjc/club_tenis/PracticaSistemasDistribuidos2025Grupo9Application.java
@@ -0,0 +1,13 @@
+package es.urjc.club_tenis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticaSistemasDistribuidos2025Grupo9Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PracticaSistemasDistribuidos2025Grupo9Application.class, args);
+	}
+
+}
---
+++ src/test/java/es/urjc/club_tenis/PracticaSistemasDistribuidos2025Grupo9ApplicationTests.java
@@ -0,0 +1,13 @@
+package es.urjc.club_tenis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticaSistemasDistribuidos2025Grupo9ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
