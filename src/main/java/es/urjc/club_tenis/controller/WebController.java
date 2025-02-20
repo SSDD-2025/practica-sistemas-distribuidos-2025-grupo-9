77b225f57bbfdbe12667f91d4cd939b8443def8b
rodriLB <rodrigo.lopezbarca@gmail.com>
2025-02-20 14:40:46 +0100

    Added user view endpoint

--- src/main/java/es/urjc/club_tenis/controller/WebController.java
+++ src/main/java/es/urjc/club_tenis/controller/WebController.java
@@ -1,16 +1,26 @@
 package es.urjc.club_tenis.controller;
 
+import es.urjc.club_tenis.model.*;
+import es.urjc.club_tenis.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.service.annotation.GetExchange;
 
 @Controller
 public class WebController {
 
-    @GetMapping("/profile")
-    public String getProfilePage(Model model){
+    @Autowired
+    private UserService userService;
+
+    @GetMapping("/profile/{username}")
+    public String getProfilePage(Model model, @PathVariable String username){
+        User user = userService.findByUsername(username);
+        model.addAttribute("user", user);
+        model.addAttribute("followingOthers", user.followedUsers.size());
+        model.addAttribute("followingMe", user.followedBy.size());
         return "profile";
     }
 
@@ -20,4 +30,5 @@ public class WebController {
         return "match";
     }
 
+    //para resesrvar pista se puede hacer con un mapa con el tiempo como clave y con un usuario como booleano para reserva
 }
