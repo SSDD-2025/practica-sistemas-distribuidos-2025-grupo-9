4b94681e889e262c91133159101e3ef51f8b7271
RuBaPr <r.bargueno.2020@alumnos.urjc.es>
2025-02-20 11:55:33 +0100

    Creacion primera version de Tournament

---
+++ src/main/java/es/urjc/club_tenis/model/Tournament.java
@@ -0,0 +1,63 @@
+package es.urjc.club_tenis.model;
+
+import jakarta.persistence.*;
+import java.util.*;
+import java.time.*;
+
+
+@Entity
+public class Tournament {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    private LocalDateTime initDate;
+    private LocalDateTime endDate;
+
+    @OneToMany
+    private List <Match> matches;
+
+    @ManyToOne
+    private User winner;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public LocalDateTime getInitDate() {
+        return initDate;
+    }
+
+    public void setInitDate(LocalDateTime initDate) {
+        this.initDate = initDate;
+    }
+
+    public LocalDateTime getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(LocalDateTime endDate) {
+        this.endDate = endDate;
+    }
+
+    public List<Match> getMatches() {
+        return matches;
+    }
+
+    public void setMatches(List<Match> matches) {
+        this.matches = matches;
+    }
+
+    public User getWinner() {
+        return winner;
+    }
+
+    public void setWinner(User winner) {
+        this.winner = winner;
+    }
+}
