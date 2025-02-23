75930bf790cbb51fa6cd59864682c1312066b72e
RuBaPr <r.bargueno.2020@alumnos.urjc.es>
2025-02-23 13:55:08 +0100

    Created first version of UserController and register.html

---
+++ src/main/java/es/urjc/club_tenis/controller/UserController.java
@@ -0,0 +1,32 @@
+package es.urjc.club_tenis.controller;
+
+import es.urjc.club_tenis.model.User;
+import es.urjc.club_tenis.service.UserService;
+import org.springframework.beans.factory.annotation.*;
+import org.springframework.stereotype.*;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+public class UserController {
+
+    @Autowired
+    private UserService service;
+
+    @GetMapping("/users/register")
+    public String showRegistrationForm(Model model) {
+        model.addAttribute("user", new User());
+        return "register";
+    }
+
+
+    @PostMapping("/users/register")
+    public String registerUser(Model model, String username, String name, String password) {
+
+        User newUser = service.save(username, name, password);
+
+        model.addAttribute("user", newUser.getId());
+
+        return "redirect:/users/" + newUser.getId();
+    }
+}
