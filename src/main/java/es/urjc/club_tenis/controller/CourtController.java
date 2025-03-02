5a4f8417cbcf02c898e3b8073e2df617ffd4a12c
ArielCB <arielcarnes123@gmail.com>
2025-03-02 23:58:23 +0100

    Create Basic Court Controller

--- src/main/java/es/urjc/club_tenis/controller/CourtController.java
+++ src/main/java/es/urjc/club_tenis/controller/CourtController.java
@@ -1,18 +1,34 @@
 package es.urjc.club_tenis.controller;
 
 
+import java.sql.Time;
+import java.time.LocalDate;
+import java.time.LocalTime;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/court")
 public class CourtController {
 
     @GetMapping("/")
-    public String getCourts(Model model, @PathVariable long id){
+    public String getCourts(Model model/*, @PathVariable long id*/){
         return "court";
+    }
+
+    @PostMapping("/book")
+    public String bookCourt(Model model, String date, String start, String end){
+
+        LocalDate newDate = LocalDate.parse(date);
+        LocalTime newStart = LocalTime.parse(start);
+        LocalTime newEnd = LocalTime.parse(end);
+
+        return "redirect:/";
     }
 }
---
+++ src/main/java/es/urjc/club_tenis/service/CourtService.java
@@ -0,0 +1,5 @@
+package es.urjc.club_tenis.service;
+
+public class CourtService {
+    
+}
