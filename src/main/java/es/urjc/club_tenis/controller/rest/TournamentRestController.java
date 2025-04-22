e76d2e08354f5732af1940f97e69ed38c090fc5a
Hugo Fernandez <hugoferod@gmail.com>
2025-04-22 18:17:23 +0200

    Update endpoints TournamentRestController

--- src/main/java/es/urjc/club_tenis/controller/rest/TournamentRestController.java
+++ src/main/java/es/urjc/club_tenis/controller/rest/TournamentRestController.java
@@ -13,40 +13,40 @@ import java.util.Collection;
 import static org.springframework.web.servlet.support.ServletUriComponentsBuilder.fromCurrentRequest;
 
 @RestController
-@RequestMapping("/tournaments")
+@RequestMapping("/api")
 public class TournamentRestController {
 
     @Autowired
     private TournamentService tournamentService;
 
-    @GetMapping("/")
+    @GetMapping("/tournaments/")
     public Collection<TournamentDTO> getAllTournaments() {
         return tournamentService.findAll();
     }
 
-    @GetMapping("/{id}")
+    @GetMapping("/tournament/{id}")
     public TournamentDTO getTournament(@PathVariable long id) {
         return tournamentService.findById(id);
     }
 
-    @PostMapping("/")
+    @PostMapping("/tournament/")
     public ResponseEntity<TournamentDTO> createTournament(@RequestBody TournamentDTO tournament) {
         TournamentDTO created = tournamentService.save(tournament);
         URI location = fromCurrentRequest().path("/{id}").buildAndExpand(created.id()).toUri();
         return ResponseEntity.created(location).body(created);
     }
 
-    @PutMapping("/{id}")
+    @PutMapping("/tournament/{id}")
     public TournamentDTO updateTournament(@PathVariable long id, @RequestBody TournamentDTO tournament) {
         return tournamentService.modify(id, tournament);
     }
 
-    @DeleteMapping("/{id}")
+    @DeleteMapping("/tournament/{id}")
     public void deleteTournament(@PathVariable long id) {
         tournamentService.delete(id);
     }
 
-    @PostMapping("/{id}/addMatch")
+    @PostMapping("/tournament/{id}/addMatch")
     public void addMatch(@PathVariable long id, @RequestBody MatchDTO match) {
         tournamentService.addMatch(id, match);
     }
