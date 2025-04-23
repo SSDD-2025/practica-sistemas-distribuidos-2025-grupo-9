8d74cb729f87356dcad0899ab067859d9d7ccee8
rodriLB <rodrigo.lopezbarca@gmail.com>
2025-04-23 14:38:08 +0200

    Simplified TournamentDTO

--- src/main/java/es/urjc/club_tenis/dto/tournament/TournamentDTO.java
+++ src/main/java/es/urjc/club_tenis/dto/tournament/TournamentDTO.java
@@ -1,6 +1,7 @@
 package es.urjc.club_tenis.dto.tournament;
 
 import es.urjc.club_tenis.dto.match.MatchDTO;
+import es.urjc.club_tenis.dto.user.UserBasicDTO;
 import es.urjc.club_tenis.dto.user.UserDTO;
 import java.time.LocalDate;
 import java.util.List;
@@ -11,7 +12,7 @@ public record TournamentDTO(
         LocalDate initDate,
         LocalDate endDate,
         int price,
-        List<UserDTO> participants,
+        List<UserBasicDTO> participants,
         List<MatchDTO> matches
 ) {
 }
